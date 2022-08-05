5a1e2ad9b7beecba4ebcc6b5973a6cb0709ff166
Pepijn Verburg <pepijn@moeilijkedingen.nl>
2022-08-05 15:03:53 +0200

    refactor: the NPM module will now not automatically start the process, but only export the process handlers. There is now a dedicated entry-point for starting the process directly to be used in the PM2 and Docker configurations.

--- src/start.ts
+++ src/start.ts
@@ -1,11 +1,21 @@
+import logger from './logger';
 import { startProcess, stopProcess } from './process';
+
+logger.info(`Starting the main process...`);
 
 process.on('SIGINT', () => {
   stopProcess();
 });
 
 (async(): Promise<void> => {
-  startProcess();
+	try {
+		await startProcess();
+		logger.info(`🚀 Ready to receive GraphQL requests.`);
+	} catch (error) {
+		logger.error(`An error occurred during startup:`);
+		logger.error(error);
+		process.exit(1);
+	}
 })();
 
 
